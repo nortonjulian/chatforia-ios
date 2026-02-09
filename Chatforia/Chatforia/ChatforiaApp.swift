2c63987d2980d369eaac7f399afdd3a1cb3e01eb
Julian Norton <nortonjulian@gmail.com>
2026-02-09 15:58:01 -0700

    Initial iOS app scaffold (SwiftUI)

---
+++ Chatforia/Chatforia/ChatforiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatforiaApp.swift
+//  Chatforia
+//
+//  Created by Julian Norton on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatforiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
